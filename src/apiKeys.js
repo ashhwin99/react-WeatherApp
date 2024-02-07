558bf23f4c25661d4fd6620aaccab72f655dfc9d
ashhwin99 <ashwinjr99@gmail.com>
2024-02-07 15:47:50 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "ab50e3433a6853aed23b6ccc79a79113",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
